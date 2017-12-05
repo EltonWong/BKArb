519142c64c75fd03339dafc0344b1246bdbbce7f
Elton Wong <eltonwong1988@gmail.com>
2017-12-04 22:25:06 -0500

    Update sampleCode.py

--- sampleCode.py
+++ sampleCode.py
@@ -1,9 +1,10 @@
+from config.py import *
 from binance.client import Client as bClient
 from kucoin.client import Client as kClient
 
 #Login Data:
-bclient = bClient('GET9kqOyfCz2fiqCDse4wZgwXHqf2BiCEoTUnWqbzTQAiXtASMnz1kmbEEXA3XbE', 'jy79DJ5ljhLaVn080Tg5dwmJcI0iUvZIQTIC48AlV3HupZ3y3B6ZdhyP2s2m1b3z')	
-kclient = kClient('5a24a009e0abb849c3933bf4', 'cbb0eae2-e6b5-4b15-91b1-3a2427a02760')
+bclient = bClient(bKey, bSecret)	
+kclient = kClient(kKey, kSecret)
 
 # get market depth
 bdepth = bclient.get_order_book(symbol='BNBBTC')
